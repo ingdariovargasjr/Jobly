8f41ba019e263e799ce91dfe9234185405e899f3
Darío Maldonado Vargas <dariovargasjr@gmail.com>
2021-02-01 13:49:08 -0600

    First Commit
    First commit

---
+++ app/src/main/java/com/example/jobly/PerfilYoberActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jobly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerfilYoberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_yober)
+    }
+}
